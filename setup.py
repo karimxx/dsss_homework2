c9f5698d92b912136acda99207eb88471dc15c77
karimxx <karin.said2000@gmail.com>
2024-11-06 14:41:45 +0000

    Add math_quiz folder to repository

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="dsss_homework2",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Your Name",
+    description="Description of the project",
+    license="Apache License 2.0",
+    url="https://github.com/karimxx/dsss_homework2.git",
+)
